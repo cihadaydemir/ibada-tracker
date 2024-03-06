7efc8fab172d4bf2dd935a088d99ef59ffbdf6c8
Cihad <cihad.aydemir@t-systems.com>
2024-03-06 23:42:41 +0100

    feat: export db connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,4 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+
+
 import postgres from "postgres";
 
 import { env } from "@/env";
@@ -12,7 +14,7 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+export const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
