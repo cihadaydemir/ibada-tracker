d9a36ed43fdb401e6e303833f631dfc8eb420519
Cihad <cihad.aydemir@t-systems.com>
2024-03-07 11:49:03 +0100

    feat: adjust create ibada component to new api

--- src/app/_components/create-ibada.tsx
+++ src/app/_components/create-ibada.tsx
@@ -1,20 +1,42 @@
 "use client"
 import { api } from "@/trpc/react"
-import { create } from "domain"
+
+import { useState } from "react"
 
 export const CreateIbada = () => {
 	const createIbada = api.ibada.create.useMutation()
+	const { data: ibadaTypes } = api.ibadaTypes.getAll.useQuery()
 	const utils = api.useUtils()
 
+	const [selectedIbadaTypeId, setSelectedIbadaTypeId] = useState<string>("")
 	return (
-		<div>
+		<div className="flex flex-col gap-2">
 			{createIbada.error && <p>Error occured: {createIbada.error.message}</p>}
+
+			<select
+				defaultValue=""
+				value={selectedIbadaTypeId}
+				onChange={(e) => {
+					e.preventDefault()
+					setSelectedIbadaTypeId(e.target.value)
+				}}
+			>
+				<option disabled value={""}>
+					Select a ibada
+				</option>
+				{ibadaTypes?.map((ibada) => (
+					<option value={ibada.id} className="text-black">
+						<p className="text-black ">{ibada.name}</p>
+					</option>
+				))}
+			</select>
 			<button
+				type="button"
 				onClick={(e) => {
 					e.preventDefault()
 					createIbada.mutate(
 						{
-							ibadaType: "PRAYER",
+							ibadaTypeId: selectedIbadaTypeId,
 							userId: 1,
 						},
 						{
