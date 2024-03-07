4cf25a0f32ffdb1e8af0bf14c4feabf98cac8579
Cihad <cihad.aydemir@t-systems.com>
2024-03-07 09:17:11 +0100

    feat: add prayer const to id fn

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,11 @@
 import { customAlphabet } from "nanoid"
 export const nanoid = customAlphabet("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
 
+// TODO change prayer to ibada
 const prefixes = {
 	user: "usr",
 	ibada: "ibd",
+	prayer: "pryr",
 } as const
 
 export function newId(prefix: keyof typeof prefixes): string {
