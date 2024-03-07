8e44b5456b4ef98e5044656d96261f7c74c66495
Cihad <cihad.aydemir@t-systems.com>
2024-03-07 09:16:11 +0100

    feat: add ibada types endpoint, adjust ibadas endpoint

--- src/server/api/routers/ibada.ts
+++ src/server/api/routers/ibada.ts
@@ -1,19 +1,26 @@
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc"
 import { db } from "@/server/db"
-import { createIbadasInputSchema, ibadas, selectIbadasSchema } from "@/server/db/schema"
+import { createIbadasInputSchema, ibadas, scores, users } from "@/server/db/schema"
 import { newId } from "@/utils"
 import { eq } from "drizzle-orm"
 
+const authUserId = 1
+
 export const ibadaRouter = createTRPCRouter({
-	getAll: publicProcedure.input(selectIbadasSchema.pick({ userId: true })).query(async ({ input }) => {
+	getAll: publicProcedure.query(async ({ input }) => {
+		//TODO replace with authenticated user later:
 		return await db.query.ibadas.findMany({
-			where: eq(ibadas.userId, input.userId),
+			where: eq(ibadas.userId, authUserId),
 		})
 	}),
 
 	create: publicProcedure
 		.input(createIbadasInputSchema.omit({ id: true, createdAt: true }))
 		.mutation(async ({ ctx, input }) => {
+			const userScores = ctx.db.query.scores.findFirst({
+				where: eq(users.id, authUserId),
+			})
+			//TODO update users score
 			return await ctx.db.insert(ibadas).values({
 				...input,
 				id: newId("ibada"),
