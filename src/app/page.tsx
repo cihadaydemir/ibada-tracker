f0065b64f4c6ef23e28fbc3149ce88919160c351
Cihad <cihad.aydemir@t-systems.com>
2024-03-07 11:48:17 +0100

    feat: add score endpoint + ui component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,19 @@ import { unstable_noStore as noStore } from "next/cache"
 
 import { api } from "@/trpc/server"
 import { CreateIbada } from "./_components/create-ibada"
+import { IbadaList } from "./_components/ibada-list"
+import { Score } from "./_components/score"
 
 export default async function Home() {
 	noStore()
 
-	const ibadas = await api.ibada.getAll.query({ userId: 1 })
-
+	const ibadas = await api.ibada.getAll.query()
+	const userScore = await api.scores.getScore.query()
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-				{ibadas.map((ibada) => (
-					<p>{ibada.ibadaType}</p>
-				))}
+				<Score initialData={userScore!} />
+				<IbadaList />
 				<CreateIbada />
 			</div>
 		</main>
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,7 +1,7 @@
-import { postRouter } from "@/server/api/routers/post"
 import { createTRPCRouter } from "@/server/api/trpc"
 import { ibadaRouter } from "./routers/ibada"
 import { ibadaTypesRouter } from "./routers/ibadaTypes"
+import { scoresRouter } from "./routers/score"
 
 /**
  * This is the primary router for your server.
@@ -9,9 +9,9 @@ import { ibadaTypesRouter } from "./routers/ibadaTypes"
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-	post: postRouter,
 	ibada: ibadaRouter,
 	ibadaTypes: ibadaTypesRouter,
+	scores: scoresRouter,
 })
 
 // export type definition of API
