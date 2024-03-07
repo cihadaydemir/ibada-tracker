ee974f74b49f9808314bc53979a676d7a1ada686
Cihad <cihad.aydemir@t-systems.com>
2024-03-07 01:08:21 +0100

    feat: add ibada trpc endpoint

---
+++ src/utils/index.ts
@@ -0,0 +1,12 @@
+import { customAlphabet } from "nanoid"
+export const nanoid = customAlphabet("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
+
+const prefixes = {
+	user: "usr",
+	ibada: "ibd",
+	
+} as const
+
+export function newId(prefix: keyof typeof prefixes): string {
+	return [prefixes[prefix], nanoid(17)].join("_")
+}
