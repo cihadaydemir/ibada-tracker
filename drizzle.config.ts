1c77a675eca8756dbe1368141dbfb866d955687c
Cihad <cihad.aydemir@t-systems.com>
2024-03-07 01:24:44 +0100

    feat: add default timestampt within drizzle schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { type Config } from "drizzle-kit";
+import type { Config } from "drizzle-kit"
 
-import { env } from "@/env";
+import { env } from "@/env"
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  out:"./src/server/db/migrations/",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: env.DATABASE_URL,
-  },
-  tablesFilter: ["ibada-tracker_*"],
-} satisfies Config;
+	schema: "./src/server/db/schema.ts",
+	out: "./src/server/db/migrations/",
+	driver: "pg",
+	dbCredentials: {
+		connectionString: env.DATABASE_URL,
+	},
+	tablesFilter: ["ibada-tracker_*"],
+} satisfies Config
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,19 +1,9 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { relations, sql } from "drizzle-orm";
-import {
-  index,
-  integer,
-  pgEnum,
-  pgTable,
-  pgTableCreator,
-  serial,
-  text,
-  timestamp,
-  varchar
-} from "drizzle-orm/pg-core";
-import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { relations, sql } from "drizzle-orm"
+import { index, integer, pgEnum, pgTable, pgTableCreator, serial, text, timestamp, varchar } from "drizzle-orm/pg-core"
+import { createInsertSchema, createSelectSchema } from "drizzle-zod"
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -21,71 +11,45 @@ import { createInsertSchema, createSelectSchema } from "drizzle-zod";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `ibada-tracker_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt"),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const ibadaTypesEnum = pgEnum("ibada-types", ["PRAYER", "FASTING", "DJUMA", "EID_PRAYER", "QURAN"])
 
-export const ibadaTypesEnum = pgEnum("ibada-types",[
-  "PRAYER",
-"FASTING",
-"DJUMA",
-"EID_PRAYER",
-"QURAN"
-])
-
-export const users = pgTable("users",{
-  id: integer("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
+export const users = pgTable("users", {
+	id: integer("id").primaryKey(),
+	name: varchar("name", { length: 256 }),
+	createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
 })
-
 
 export const usersRelations = relations(users, ({ many }) => ({
-  ibada: many(ibadas),
-}));
-
-
-
-export const ibadas = pgTable("ibadas",{
-  id: text("id").primaryKey(),
-  ibadaType: ibadaTypesEnum('ibada-types').notNull(),
-  createdAt: timestamp("created_at")
-  .default(sql`CURRENT_TIMESTAMP`)
-  .notNull(),
-  userId: integer("user_id").notNull().references(()=> users.id,{ onDelete: "cascade" })
-})
-
-export const createIbadasInputSchema = createInsertSchema(ibadas);
-export const selectIbadasSchema = createSelectSchema(ibadas);
-
-export const ibadaRelations = relations(ibadas, ({one})=>({
-  user: one(users,{
-    fields:[ibadas.userId],
-    references:[users.id]
-  })
-}));
-
-export const userScoreRelations = relations(users,({one})=>({
-  scores: one(scores)
+	ibada: many(ibadas),
 }))
 
-export const scores = pgTable("scores",{
-        userId: integer("user_id").notNull().references(()=> users.id,{ onDelete: "cascade" }),
-        score: integer('score')
+export const ibadas = pgTable("ibadas", {
+	id: text("id").primaryKey(),
+	ibadaType: ibadaTypesEnum("ibada-types").notNull(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	userId: integer("user_id")
+		.notNull()
+		.references(() => users.id, { onDelete: "cascade" }),
+})
 
+export const createIbadasInputSchema = createInsertSchema(ibadas)
+export const selectIbadasSchema = createSelectSchema(ibadas)
+
+export const ibadaRelations = relations(ibadas, ({ one }) => ({
+	user: one(users, {
+		fields: [ibadas.userId],
+		references: [users.id],
+	}),
+}))
+
+export const userScoreRelations = relations(users, ({ one }) => ({
+	scores: one(scores),
+}))
+
+export const scores = pgTable("scores", {
+	userId: integer("user_id")
+		.notNull()
+		.references(() => users.id, { onDelete: "cascade" }),
+	score: integer("score"),
 })
