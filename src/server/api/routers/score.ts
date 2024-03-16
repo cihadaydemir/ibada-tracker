582957e530dc77b856becb519d963a1fe8454347
Cihad <cihad.aydemir@t-systems.com>
2024-03-16 05:34:44 +0100

    feat(scores): WIP find or create score for auth user

--- src/server/api/routers/score.ts
+++ src/server/api/routers/score.ts
@@ -4,9 +4,19 @@ import { scores } from "@/server/db/schema"
 import { eq } from "drizzle-orm"
 
 export const scoresRouter = createTRPCRouter({
-	getScore: protectedProcedure.query(async ({ input, ctx }) => {
-		return await db.query.scores.findFirst({
+	getScore: protectedProcedure.query(async ({ ctx }) => {
+		const score = await db.query.scores.findFirst({
 			where: eq(scores.userId, ctx.user.id),
 		})
+		if (!score) {
+			return await db
+				.insert(scores)
+				.values({
+					userId: ctx.user.id,
+					score: 0,
+				})
+				.returning()
+		}
+		return score
 	}),
 })
