25015ce94a771fa8ec24394ed5148f35c331d226
Cihad <cihad.aydemir@t-systems.com>
2024-03-17 03:38:41 +0100

    feat(user): add user endpoint to trpc

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "@/server/api/trpc"
 import { ibadaRouter } from "./routers/ibada"
 import { ibadaTypesRouter } from "./routers/ibadaTypes"
 import { scoresRouter } from "./routers/score"
+import { userRouter } from "./routers/user"
 
 /**
  * This is the primary router for your server.
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
 	ibada: ibadaRouter,
 	ibadaTypes: ibadaTypesRouter,
 	scores: scoresRouter,
+	user: userRouter,
 })
 
 // export type definition of API
