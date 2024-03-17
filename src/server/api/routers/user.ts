ea6ba03b69963a415a58c4068d2c5dce8cbec967
Cihad <cihad.aydemir@t-systems.com>
2024-03-17 03:38:41 +0100

    feat(user): add user endpoint to trpc

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,28 @@
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc"
+import { scores, users } from "@/server/db/schema"
+
+export const userRouter = createTRPCRouter({
+	findOrCreateUser: protectedProcedure.mutation(async ({ ctx }) => {
+		const tableUser = await ctx.db.query.users.findFirst({
+			where: (table, { eq }) => eq(users.id, table.id),
+		})
+		if (!tableUser) {
+			const newUser = (
+				await ctx.db
+					.insert(users)
+					.values({
+						id: ctx.user.id,
+						name: ctx.user.email,
+					})
+					.returning()
+			)[0]
+
+			await ctx.db.insert(scores).values({
+				userId: newUser?.id!,
+				score: 0,
+			})
+			return newUser
+		}
+		return tableUser
+	}),
+})
