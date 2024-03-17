ca4306d54ff863cc4915848b591b257e0790f243
Cihad <cihad.aydemir@t-systems.com>
2024-03-17 03:46:56 +0100

    feat(header): add header with logout btn

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -5,8 +5,9 @@ import { useRouter } from "next/navigation"
 
 export const Header = () => {
 	const router = useRouter()
+
 	return (
-		<div>
+		<div className="flex flex-row gap-2 justify-center items center">
 			<Button
 				className="bg-primary text-primary-foreground"
 				onClick={async (e) => {
