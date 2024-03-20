3bfccfb760e232d8640f4205d42894c357952668
Cihad <cihad.aydemir@t-systems.com>
2024-03-21 00:36:40 +0100

    refactor(ibada): turn ibada list to client component to fix revalidation issue

--- src/app/_components/ibada-list.tsx
+++ src/app/_components/ibada-list.tsx
@@ -1,9 +1,10 @@
-import { api } from "@/trpc/server"
+"use client"
 
+import { api } from "@/trpc/react"
 import IbadaItem from "./ibada-item"
 
-export const IbadaList = async () => {
-	const data = await api.ibada.getAll.query()
+export const IbadaList = () => {
+	const { data } = api.ibada.getAll.useQuery()
 	return (
 		<div>
 			{data?.map((ibada) => (
