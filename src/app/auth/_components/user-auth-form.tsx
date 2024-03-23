dac2af14441b9024c19274e343e2c96f6a8b12f4
Cihad <cihad.aydemir@t-systems.com>
2024-03-23 03:42:47 +0100

    feat: WIP adjust auth page

--- src/app/auth/_components/user-auth-form.tsx
+++ src/app/auth/_components/user-auth-form.tsx
@@ -1,126 +1,25 @@
 "use client"
 import type * as React from "react"
 
-import { Icons } from "@/app/_components/icons"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { SignUpForm } from "./RegisterForm"
+import { LoginForm } from "./SignInForm"
 
-import { supabaseFrontendClient } from "@/lib/supabase/client"
-
-import { useToast } from "@/components/ui/use-toast"
-import { redirect, useRouter } from "next/navigation"
-import { useState } from "react"
-
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-	const { toast } = useToast()
-	const router = useRouter()
-
-	const [isLoading, setIsLoading] = useState(false)
-	const [emailInput, setEmailInput] = useState("")
-	const [passwordInput, setPasswordInput] = useState("")
-
-	async function handleSignUp() {
-		setIsLoading(true)
-
-		const { data, error } = await supabaseFrontendClient.auth.signUp({
-			email: emailInput,
-			password: passwordInput,
-			options: {
-				emailRedirectTo: [window.location.origin, "/auth/confirm"].join(""),
-			},
-		})
-		setIsLoading(false)
-
-		if (!error) {
-			router.push("/auth/confirmation")
-		} else {
-			toast({
-				title: "Registration failed! ❌",
-				description: "Something went rong! Please try again.",
-				variant: "destructive",
-			})
-		}
-		return data.user
-	}
-	async function handleLogin() {
-		setIsLoading(true)
-		const { data, error } = await supabaseFrontendClient.auth.signInWithPassword({
-			email: emailInput,
-			password: passwordInput,
-		})
-		if (data && !error) {
-			router.push("/")
-		}
-		setIsLoading(false)
-		return data.user
-	}
-
+export function UserAuthForm() {
 	return (
-		<div className="flex flex-col gap-4">
-			<form>
-				<div className="grid gap-2">
-					<div className="grid gap-1">
-						<Label className="sr-only" htmlFor="email">
-							Email
-						</Label>
-						<Input
-							id="email"
-							placeholder="name@example.com"
-							type="email"
-							autoCapitalize="none"
-							autoComplete="email"
-							autoCorrect="off"
-							value={emailInput}
-							onChange={(e) => setEmailInput(e.target.value)}
-						/>
-
-						<Label className="sr-only" htmlFor="password">
-							Password
-						</Label>
-						<Input
-							id="password"
-							type="password"
-							value={passwordInput}
-							onChange={(e) => setPasswordInput(e.target.value)}
-						/>
-					</div>
-					<Button
-						className="bg-secondary text-secondary-foreground"
-						disabled={isLoading}
-						onClick={async (e) => {
-							e.preventDefault()
-							await handleLogin()
-						}}
-					>
-						Login with Email
-					</Button>
-					<Button
-						className="bg-primary text-primary-foreground"
-						disabled={isLoading}
-						onClick={async (e) => {
-							e.preventDefault()
-							await handleSignUp()
-						}}
-					>
-						Sign Up with Email
-					</Button>
-				</div>
-			</form>
-			<div className="relative">
-				<div className="absolute inset-0 flex items-center">
-					<span className="w-full border-t" />
-				</div>
-				<div className="relative flex justify-center text-xs uppercase">
-					<span className="bg-background px-2 text-muted-foreground">Or continue with</span>
-				</div>
-			</div>
-			<Button variant="outline" type="button">
-				<Icons.gitHub className="mr-2 h-4 w-4" />
-				GitHub
-			</Button>
+		<div>
+			<Tabs defaultValue="signup" className="w-[400px]">
+				<TabsList>
+					<TabsTrigger value="signup">Signup</TabsTrigger>
+					<TabsTrigger value="login">Login</TabsTrigger>
+				</TabsList>
+				<TabsContent value="signup">
+					<SignUpForm />
+				</TabsContent>
+				<TabsContent value="login">
+					<LoginForm />
+				</TabsContent>
+			</Tabs>
 		</div>
 	)
 }
