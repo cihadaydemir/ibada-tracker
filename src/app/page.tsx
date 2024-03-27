e0d9e296fee2db6a46472e475a4ae9d722a55baa
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2024-03-27 23:01:49 +0100

    feat: display user name in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-				{user.email}
+				<h2>Hallo, {user.user_metadata.firstname}</h2>
 				<Header />
 				<Score initialData={userScore!} />
 				<IbadaList />
