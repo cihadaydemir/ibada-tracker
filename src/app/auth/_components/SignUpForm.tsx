fe735ee039b0613ca54bd3cb65e7c159d888f61e
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2024-03-27 23:00:04 +0100

    refactor: rename component

--- src/app/auth/_components/SignUpForm.tsx
+++ src/app/auth/_components/SignUpForm.tsx
@@ -16,8 +16,8 @@ export const SignUpForm = () => {
 
 	const signUpFormSchema = z.object({
 		email: z.string().email(),
-		firstname: z.string().min(6),
-		lastname: z.string().min(6),
+		firstname: z.string().min(1),
+		lastname: z.string().min(1),
 		password: z.string().min(6),
 	})
 	const form = useForm<z.infer<typeof signUpFormSchema>>({
@@ -30,6 +30,10 @@ export const SignUpForm = () => {
 			password: values.password,
 			options: {
 				emailRedirectTo: [window.location.origin, "/auth/confirm"].join(""),
+				data: {
+					firstname: values.firstname,
+					lastname: values.lastname,
+				},
 			},
 		})
 
